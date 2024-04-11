d476b4b1581b77c2ec33f24ffd2483bda58ef915
Leon Dammer <l.dammer@gmx.de>
2024-04-11 14:44:15 +0200

    replaced messages with warnings if the needed input/column was not found

--- R/mosdef_check_inputs.R
+++ R/mosdef_check_inputs.R
@@ -15,41 +15,41 @@ mosdef_res_check <- function(res_de,
   if ("symbol" %in% colnames(res_de)) {
     if (verbose) message("Found a 'symbol' column!")
   } else {
-    message("Could not find a 'symbol' column. This can be created for you in the other functions")
+    warning("Could not find a 'symbol' column. This can be created for you in the other functions")
   }
 
   if ("padj" %in% colnames(res_de)) {
     if (verbose) message("Found a 'padj' column!")
   } else {
-    message("Could not find a 'padj' column. Please ensure you have these values
+    warning("Could not find a 'padj' column. Please ensure you have these values
           and rename the column: 'padj'")
   }
 
   if ("log2FoldChange" %in% colnames(res_de)) {
     if (verbose) message("Found a 'log2FoldChange' column!")
   } else {
-    message("Could not find a 'log2FoldChange' column. Please ensure you have these values
+    warning("Could not find a 'log2FoldChange' column. Please ensure you have these values
           and rename the column: 'log2FoldChange'")
   }
 
   if ("pvalue" %in% colnames(res_de)) {
     if (verbose) message("Found a 'pvalue' column!")
   } else {
-    message("Could not find a 'pvalue' column. Please ensure you have these values
+    warning("Could not find a 'pvalue' column. Please ensure you have these values
           and rename the column: 'pvalue'")
   }
 
   if ("pvalue" %in% colnames(res_de)) {
     if (verbose) message("Found a 'baseMean' column!")
   } else {
-    message("Could not find a 'baseMean' column. If you want to use the gene_plot function:
+    warning("Could not find a 'baseMean' column. If you want to use the gene_plot function:
           Please ensure you have these values and rename the column: 'baseMean'")
   }
 
   if (all(grep(pattern = "^ENS", rownames(res_de)))) {
     if (verbose) message("Rownames are ENSEMBL IDs.")
   } else {
-    message("Rownames are not ENSEMBL IDs. Please change them to ENSEMBL IDs by using
+    warning("Rownames are not ENSEMBL IDs. Please change them to ENSEMBL IDs by using
             AnnotationDbis 'mapIDs'")
   }
 
@@ -81,14 +81,14 @@ mosdef_dds_check <- function(dds,
   if (all(grep(pattern = "^ENS", rownames(dds)))) {
     if (verbose) message("Rownames are ENSEMBL IDs.")
   } else {
-    message("Rownames are not ENSEMBL IDs. Please change them to ENSEMBL IDs by using
+    warning("Rownames are not ENSEMBL IDs. Please change them to ENSEMBL IDs by using
             AnnotationDbis 'mapIDs'")
   }
   if ("counts" %in% names(assays(dds))) {
 
     if (verbose) message("Found a 'counts' assay!")
   } else {
-    message("Could not find a 'counts' assay Please ensure you have these values
+    warning("Could not find a 'counts' assay Please ensure you have these values
           and rename the column/assay: 'counts'")
   }
   return(invisible(NULL))
